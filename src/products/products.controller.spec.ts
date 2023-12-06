54389eee9988f6394a707c302042a32c36c1fb94
Anabel <assannanabel@gmail.com>
2023-12-06 13:48:57 -0300

    update y delete ok

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-//import { Product } from './entities/product.entity';
+import { Product } from './entities/product.entity';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
@@ -36,19 +36,44 @@ describe('ProductsController', () => {
     const productSpy=jest.spyOn(service,"findById");
     productSpy.mockImplementation(() =>result);
     expect(controller.getProductById("1")).toBe(result);
-    productSpy.mockRestore();
+    productSpy.mockRestore();// para limpiar
   });
 
   it("should return the create new product (use spyOn)",
   () =>{
    const newProduct={id:'8',product_name: 'Amoladora', description: 'Amoladora a bateria', price:10456 };
    const productSpy=jest.spyOn(service,"create").mockReturnValue(newProduct);//spyOn espia el metodo create del servicio
-   const result=controller.createProduct(newProduct)
+   const result=controller.createProduct(newProduct)// se llama al metodo del controlador.
    expect(result).toEqual(newProduct);
    productSpy.mockRestore();
  });
 
+ it("should return the updated product (use spyOn)", () => {
+  const updatedProduct: Product = {
+    id: '1',
+    product_name: 'Martillo Actualizado',
+    description: 'Descripción del martillo actualizado',
+    price: 150,
+  };
 
- 
+  const productSpy = jest.spyOn(service, 'update').mockReturnValue(updatedProduct);//mockreturnvalue para establecer el valor que se devolvera cuando se llae al metodo update
+  const result =controller.updateProduct('1', updatedProduct); // llama al metodo del controlador
+  expect(result).toEqual(updatedProduct); //toEqual que sea igual
+  productSpy.mockRestore();
+});
+
+it("should return the deleted product (use spyOn)", () => {
+  const deletedProduct: Product = {
+    id: '1',
+    product_name: 'Martillo',
+    description: 'Martillo de carpintero',
+    price: 3987,
+  };
+
+  const productSpy = jest.spyOn(service, 'delete').mockReturnValue(deletedProduct);
+  const result = controller.deleteProduct('1');
+  expect(result).toEqual(deletedProduct);
+  productSpy.mockRestore();
+});
 
 });
