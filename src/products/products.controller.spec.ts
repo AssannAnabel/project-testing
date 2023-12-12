0499573359b190b94f781febd00168983a03d833
Anabel <assannanabel@gmail.com>
2023-12-12 18:06:57 -0300

    service.spec ok

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -3,89 +3,95 @@ import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { Product } from './entities/product.entity';
 
+
 describe('ProductsController', () => {
   let controller: ProductsController;
-  let service:ProductsService;
+  let service: ProductsService;
   // let mockedProductsValue= [{ id: '1', product_name: 'Martillo', description: 'Martillo de carpintero', price: 3987 },];
   // let mockProductsService={
   //   products:()=> mockedProductsValue, // es una funcion que nos retorna lo que tiene el mockedProductsValue
   // };
-
   beforeEach(async () => { //mockeando
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
       providers: [ProductsService],
     })
-    // .overrideProvider(mockProductsService)// saltear nuestro servicio real
-    // .useValue(mockedProductsValue) 
-    .compile();
-
+      // .overrideProvider(mockProductsService)// saltear nuestro servicio real
+      // .useValue(mockedProductsValue)
+      .compile();
     controller = module.get<ProductsController>(ProductsController);
     service = module.get<ProductsService>(ProductsService);
   });
-// en la filmina Mocking aca va la declaracion del test, configuracion del valor que esperamos, mockear el metodo
- 
+  // en la filmina Mocking aca va la declaracion del test, configuracion del valor que esperamos, mockear el metodo
   it('should be defined', () => {
-  expect(controller).toBeDefined();
-});
-// Agregamos findAll
-
-it("should return all products", () => {
-  const allProducts: Product[] = [
-    { id: '1', product_name: 'Martillo', description: 'Martillo de carpintero', price: 3987 },
-    { id: '2', product_name: 'Destornillador', description: 'Destornillador Phillips', price: 1499 },];
-
-  const productSpy = jest.spyOn(service, 'findAll').mockReturnValue(allProducts);
-  const result = controller.getAllProducts();
-  expect(result).toEqual(allProducts);
-  productSpy.mockRestore();
-});
-
-
-  it("should return the getById (use spyOn)",
-   () =>{
-    const result={ id: '1', product_name: 'Martillo', description: 'Martillo de carpintero', price: 3987 }
-    const productSpy=jest.spyOn(service,"findById");
-    productSpy.mockImplementation(() =>result);
-    expect(controller.getProductById("1")).toBe(result)
-    productSpy.mockRestore();// para limpiar
+    expect(controller).toBeDefined();
   });
 
-  it("should return the create new product (use spyOn)",
-  () =>{
-   const newProduct={id:'8',product_name: 'Amoladora', description: 'Amoladora a bateria', price:10456 };
-   const productSpy=jest.spyOn(service,"create").mockReturnValue(newProduct);//spyOn espia el metodo create del servicio
-   const result=controller.createProduct(newProduct)// se llama al metodo del controlador.
-   expect(result).toEqual(newProduct);
-   productSpy.mockRestore();
- });
 
- it("should return the updated product (use spyOn)", () => {
-  const updatedProduct: Product = {
-    id: '1',
-    product_name: 'Martillo Actualizado',
-    description: 'Descripción del martillo actualizado',
-    price: 150,
-  };
+  // Agregamos findAll
+  describe('testing over all products (findAll)', () => {
+    it("should return all products", () => {
+      const allProducts: Product[] = [
+        { id: '1', product_name: 'Martillo', description: 'Martillo de carpintero', price: 3987 },
+        { id: '2', product_name: 'Destornillador', description: 'Destornillador Phillips', price: 1499 },];
+      const productSpy = jest.spyOn(service, 'findAll').mockReturnValue(allProducts);
+      const result = controller.getAllProducts();
+      expect(result).toEqual(allProducts);
+      productSpy.mockRestore();
+    });
+  })
 
-  const productSpy = jest.spyOn(service, 'update').mockReturnValue(updatedProduct);//mockreturnvalue para establecer el valor que se devolvera cuando se llae al metodo update
-  const result =controller.updateProduct('1', updatedProduct); // llama al metodo del controlador
-  expect(result).toEqual(updatedProduct); //toEqual que sea igual
-  productSpy.mockRestore();
-});
 
-it("should return the deleted product (use spyOn)", () => {
-  const deletedProduct: Product = {
-    id: '1',
-    product_name: 'Martillo',
-    description: 'Martillo de carpintero',
-    price: 3987,
-  };
+  describe('Testing over get element by id (findOne)', () => {
+    it("should return the getById (use spyOn)",
+      () => {
+        const result = { id: '1', product_name: 'Martillo', description: 'Martillo de carpintero', price: 3987 }
+        const productSpy = jest.spyOn(service, "findById");
+        productSpy.mockImplementation(() => result);
+        expect(controller.getProductById("1")).toBe(result)
+        productSpy.mockRestore();// para limpiar
+      });
+  })
 
-  const productSpy = jest.spyOn(service, 'delete').mockReturnValue(deletedProduct);
-  const result = controller.deleteProduct('1');
-  expect(result).toEqual(deletedProduct);
-  productSpy.mockRestore();
-});
+  describe('Testing over create a new product (create)', () => {
+    it("should return the create new product (use spyOn)",
+      () => {
+        const newProduct = { id: '8', product_name: 'Amoladora', description: 'Amoladora a bateria', price: 10456 };
+        const productSpy = jest.spyOn(service, "create").mockReturnValue(newProduct);//spyOn espia el metodo create del servicio
+        const result = controller.createProduct(newProduct)// se llama al metodo del controlador.
+        expect(result).toEqual(newProduct);
+        productSpy.mockRestore();
+      });
+  });
 
+  describe('Testing over updated product (update)', () => {
+    it("should return the updated product (use spyOn)", () => {
+      const updatedProduct: Product = {
+        id: '1',
+        product_name: 'Martillo Actualizado',
+        description: 'Descripción del martillo actualizado',
+        price: 150,
+      };
+      const productSpy = jest.spyOn(service, 'update').mockReturnValue(updatedProduct);//mockreturnvalue para establecer el valor que se devolvera cuando se llae al metodo update
+      const result = controller.updateProduct('1', updatedProduct); // llama al metodo del controlador
+      expect(result).toEqual(updatedProduct); //toEqual que sea igual
+      productSpy.mockRestore();
+    });
+  });
+
+  
+  describe('Testing over delete method (delete)', () => {
+    it("should return the deleted product (use spyOn)", () => {
+      const deletedProduct: Product = {
+        id: '1',
+        product_name: 'Martillo',
+        description: 'Martillo de carpintero',
+        price: 3987,
+      };
+      const productSpy = jest.spyOn(service, 'delete').mockReturnValue(deletedProduct);
+      const result = controller.deleteProduct('1');
+      expect(result).toEqual(deletedProduct);
+      productSpy.mockRestore();
+    });
+  })
 });
--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -2,80 +2,99 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
 import { Product } from './entities/product.entity';
 
+
 describe('ProductsService', () => {
   let productService: ProductsService;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ProductsService],
     }).compile(); // INstanciar producsService = new ProductsService
-
     productService = module.get<ProductsService>(ProductsService);
   });
+
 
   it('should be defined', () => {
     expect(productService).toBeDefined();//validación automática que existe
   });
 
-// agregamos findAll
- it('should return all products', () => {
-   const allProducts: Product[] = [
-{ id: '1', product_name: 'Martillo', description: 'Martillo de carpintero', price: 3987 },
-{ id: '2', product_name: 'Destornillador', description: 'Destornillador Phillips', price: 1499 }];
-jest.spyOn(productService, 'findAll').mockReturnValue(allProducts);
-const result = productService.findAll();
-expect(result).toEqual(allProducts);
- });
-
-  // find By ID
-  it('should find a product by ID', () =>{ //validación buscando por id
-    const product:Product = productService.findById('1');
-    expect(product.id).toBe('1');
-  });
-// Crear nuevo producto
-  it('should create a new product', () => {
-    const newProduct: Product = {
-      id: '9',
-      product_name: 'juego de tubos',
-      description: 'juego de 10 tubos, con diferentes medidas ',
-      price: 9865,
-    };
-    const createdProduct = productService.create(newProduct);
-    expect(createdProduct.id).toBe('9');
-  });
-//modificar producto
-  it('should update an existing product', () => {
-    const updatedProduct: Product = {
-      id: '1',
-      product_name: 'Martillo Actualizado',
-      description: 'Martillo Actualizado',
-      price: 5987,
-    };
-    const result = productService.update('1', updatedProduct);
-    expect(result.product_name).toBe('Martillo Actualizado');
-  });
-
-  it('should return product not update',()=>{
-    const updatedProduct: Product = {
-      id: '9',
-      product_name: 'Martillo con id inexistente',
-      description: 'Martillo con id inexistente',
-      price: 5987,
-    };
-    const result = productService.update('9',updatedProduct)
-    expect(result).toBeNull()
+  describe('Testing over findAll', () => {
+    // agregamos findAll
+    it('should return all products', () => {
+      const allProducts: Product[] = [
+        { id: '1', product_name: 'Martillo', description: 'Martillo de carpintero', price: 3987 },
+        { id: '2', product_name: 'Destornillador', description: 'Destornillador Phillips', price: 1499 }];
+      jest.spyOn(productService, 'findAll').mockReturnValue(allProducts);
+      const result = productService.findAll();
+      expect(result).toEqual(allProducts);
+    });
+    it('get all products',()=>{
+      const mockProduct: Product[] = [
+        { id: '1', product_name: 'Martillo', description: 'Martillo de carpintero', price: 3987 },
+        { id: '2', product_name: 'Destornillador', description: 'Destornillador Phillips', price: 1499 }]
+      const allProducts = productService.findAll()
+      expect(allProducts.length).toBe(7);
+      expect(allProducts.length).toBeGreaterThan(4);
+      jest.spyOn(productService,'findAll').mockImplementationOnce(()=> mockProduct)
+      expect(allProducts).not.toContainEqual(mockProduct)
+    })
   })
 
-  // eliminar producto
-  it('should delete a product by ID', () => {
-    const deletedProduct = productService.delete('2');
-    expect(deletedProduct.id).toBe('2');
-  });
+  describe('Testing over findOne', () => {
+    // find By ID
+    it('should find a product by ID', () => { //validación buscando por id
+      const product: Product = productService.findById('1');
+      expect(product.id).toBe('1');
+    });
+  })
 
-  it('should return null if the product id does not exist', () => {
-    const notExistProductId = '999'; // ID que no existe en el arreglo de productos
-    const result = productService.delete(notExistProductId);
-    expect(result).toBeNull();
-  });
+  describe('Testing over create', () => {
+    // Crear nuevo producto
+    it('should create a new product', () => {
+      const newProduct: Product = {
+        id: '9',
+        product_name: 'juego de tubos',
+        description: 'juego de 10 tubos, con diferentes medidas ',
+        price: 9865,
+      };
+      const createdProduct = productService.create(newProduct);
+      expect(createdProduct.id).toBe('9');
+    });
+  })
+
+  describe('Testing over update', () => {
+    //modificar producto
+    it('should update an existing product', () => {
+      const updatedProduct: Product = {
+        id: '1',
+        product_name: 'Martillo Actualizado',
+        description: 'Martillo Actualizado',
+        price: 5987,
+      };
+      const result = productService.update('1', updatedProduct);
+      expect(result.product_name).toBe('Martillo Actualizado');
+    })
+    it('should return product not update', () => {
+      const updatedProduct: Product = {
+        id: '9',
+        product_name: 'Martillo con id inexistente',
+        description: 'Martillo con id inexistente',
+        price: 5987,
+      };
+      const result = productService.update('9', updatedProduct)
+      expect(result).toBeNull()
+    })
+  })
   
+  describe('Testing over delete', () => {
+    // eliminar producto
+    it('should delete a product by ID', () => {
+      const deletedProduct = productService.delete('2');
+      expect(deletedProduct.id).toBe('2');
+    });
+    it('should return null if the product id does not exist', () => {
+      const notExistProductId = '999'; // ID que no existe en el arreglo de productos
+      const result = productService.delete(notExistProductId);
+      expect(result).toBeNull();
+    });
+  })
 });
